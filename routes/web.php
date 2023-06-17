246449906654081a0960ea9b8bf521c09a0c00bd
Pedro Bonett <gilbonett@gmail.com>
2023-06-17 18:44:18 -0300

    creating routes for users

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/interactions', [InteractionsController::class, 'index'])->name('interactions.index');
 });
 
 require __DIR__.'/auth.php';
