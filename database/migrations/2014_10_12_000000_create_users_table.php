6662ba78d8900054e2c5495751a63d47ba397c6f
Pedro Bonett <gilbonett@gmail.com>
2023-06-17 18:40:53 -0300

    migrations for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cpf')->unique();
+            $table->string('phone');
+            $table->string('cep');
+            $table->date('birth');
             $table->rememberToken();
             $table->timestamps();
         });
