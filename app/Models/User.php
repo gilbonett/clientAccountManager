c7ec36533ec378da00cc4ebed246d70fde16c248
Pedro Bonett <gilbonett@gmail.com>
2023-06-17 18:41:38 -0300

    user model settings

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name',
         'email',
         'password',
+        'cpf',
+        'phone',
+        'cep',
+        'birth'
     ];
 
     /**
